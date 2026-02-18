cef6f9db8f289348c8deac46698f3e03701e3739
Joy Goswami <joygoswaminiloy2023@gmail.com>
2026-02-19 00:54:00 +0600

    get bonus implemented

---
+++ get Bonus.js
@@ -0,0 +1,29 @@
+document.getElementById('bonus_btn').addEventListener("click",()=>{
+
+const couponNum=document.getElementById("coupon_num").value;
+console.log(couponNum);
+
+const main_b=document.getElementById('cb')//"1234"
+const current_b=Number(main_b.innerText);
+console.log(current_b,typeof(current_b))
+
+
+
+if(couponNum!="69" && couponNum!="64"&& couponNum!="160" && couponNum!="94"){
+alert("Wrong Coupon Number");
+return;
+}
+else if(couponNum==="94"){
+     let new_b=current_b-current_b;
+    main_b.innerText=new_b;
+}
+else{
+    let new_b=current_b+50;
+    main_b.innerText=new_b;
+}
+
+
+
+
+
+})
--- menu.js
+++ menu.js
@@ -2,16 +2,26 @@ function showAdd() {
     add_m.classList.remove('hidden');
     withdraw_m.classList.add('hidden');
      transfer_m.classList.add("hidden");
+         getbonus_m.classList.add('hidden');
 }
 
 function showWithdraw() {
     withdraw_m.classList.remove('hidden');
     add_m.classList.add('hidden');
     transfer_m.classList.add("hidden");
+        getbonus_m.classList.add('hidden');
 }
 
 function showtransfer(){
-    transfer_m.classList.remove("hidden");
+    transfer_m.classList.remove('hidden');
+     add_m.classList.add('hidden');
+      withdraw_m.classList.add('hidden');
+        getbonus_m.classList.add('hidden');
+}
+
+function showgetbonus(){
+    getbonus_m.classList.remove('hidden');
+     transfer_m.classList.add('hidden');
      add_m.classList.add('hidden');
       withdraw_m.classList.add('hidden');
 }
